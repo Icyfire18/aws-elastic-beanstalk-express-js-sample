66747da55f9df23230d19061285f7df29a2e743c
IcyFire <armaan.17cs@kct.ac.in>
2021-04-16 12:26:02 +0530

    included apps

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Icyfire'));
+app.get('/', (req, res) => res.send('Welcome to Icyfire, This is a total exection with aws codebuil, beanstack, codepipeline, and Github '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
